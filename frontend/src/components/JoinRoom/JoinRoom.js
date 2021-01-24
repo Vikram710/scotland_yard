49c20e34d1fcc7e842b90ea082ee53de7b130612
sundar-03 <56792860+sundar-03@users.noreply.github.com>
2021-01-24 18:13:44 +0530

    Added username at join room

--- frontend/src/components/JoinRoom/JoinRoom.js
+++ frontend/src/components/JoinRoom/JoinRoom.js
@@ -1,14 +1,18 @@
 import React, {useState} from 'react';
 import {Button, Form, Col} from 'react-bootstrap';
 import {useToasts} from 'react-toast-notifications';
+import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 import {API_URL} from '../../config';
 
-const userId = localStorage.getItem('user_id');
 
 export const JoinRoom = (props) => {
 	const [roomCode, setRoomCode] = useState(null);
-
+	const [username, setName] = useState("");
+	function handleUsername(e)
+	{
+		setName(e.target.value);
+	}
 	const handleInput = (e) => {
 		const value = e.target.value;
 		setRoomCode(value);
@@ -18,11 +22,11 @@ export const JoinRoom = (props) => {
 
 		try {
 			const respose = await axios.post(`${API_URL}room/join/${roomCode}`, {
-				userId: userId,
+				userName: username,
 			});
 
 			if (respose.data.status_code === 200) {
-				window.location.href = '/game/' + roomCode;
+				window.location.href = '/game/' + roomCode+'/'+username;
 			}
 		} catch (err) {
 			console.log(err);
@@ -41,6 +45,13 @@ export const JoinRoom = (props) => {
 				<Form.Label>Password</Form.Label>
 				<Col sm="10">
 					<Form.Control type="password" placeholder="Password" />
+				</Col>
+			</Form.Group>
+
+			<Form.Group controlId="forUsername">
+				<Form.Label>Username</Form.Label>
+				<Col sm="10">
+					<TextField required id="username" type="text" label="username" onChange ={handleUsername} variant="outlined"/>
 				</Col>
 			</Form.Group>
 			<Button onClick={handleSubmit}> JOIN </Button>
